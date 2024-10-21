62f20b691b52f571d415e37bfb3932d5e551cd74
willbowden <willbowden03@yahoo.co.uk>
2024-10-21 12:11:48 +0800

    Map not showing

--- speedbands/app/page.tsx
+++ speedbands/app/page.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from "react";
-import Map from "@/components/Map";
 import dynamic from "next/dynamic";
+import styles from "./page.module.css";
 
 export default function Home() {
   const Map = useMemo(() => dynamic(
@@ -12,7 +12,7 @@ export default function Home() {
   ), [])
 
   return (
-    <div>
+    <div className={styles.mapContainer}>
       <Map position={[1.28960592759792, 103.84835955306676]} zoom={12}/>
     </div>
   );
--- speedbands/components/Map.tsx
+++ speedbands/components/Map.tsx
@@ -1,4 +1,6 @@
-import { MapContainer, Marker, Popup, TileLayer, Tooltip } from "react-leaflet"
+"use client"
+
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
 import "leaflet/dist/leaflet.css"
 import "leaflet-defaulticon-compatibility"
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
